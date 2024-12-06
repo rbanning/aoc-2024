8f56fc5ff1a4f83b9622bb1b0dbac9f7a9f693ff
rbanning <rob@myhallpass.com>
2024-12-06 05:24:58 -0800

    day three completed

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,10 +1,28 @@
+import { parsers } from '../../helpers/parsers.ts';
 import { readData, outputHeading, outputAnswer, Verbose } from '../../shared.ts';
 
 const verbose = new Verbose();
 
+function parseValidCommands(input: string) {
+  const regex = /(mul\(\d{1,3},\d{1,3}\))/g;
+  const matches = input.matchAll(regex);  
+  return Array.from(matches, (m) => m[0]);
+}
+function execCommand(command: string): number {
+  command = command.substring('mul('.length); //remove front
+  command = command.substring(0, command.length-1);
+  return command
+    .split(',')
+    .reduce((sum, str) => sum*parsers.toIntAssert(str), 1);
+}
+
 export async function day3a(dataPath?: string) {
   const data = (await readData(dataPath)).filter(Boolean);
-  return data.reduce((sum, curr) => sum + 1, 0);  
+
+  //combine into one line of text and parse
+  const commands = parseValidCommands(data.join(''))
+  return commands.reduce((sum, cmd) => sum + execCommand(cmd), 0);
+
 }
 
 Verbose.setActive(false);
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,10 +1,50 @@
+import { parsers } from '../../helpers/parsers.ts';
 import { readData, outputHeading, outputAnswer, Verbose } from '../../shared.ts';
 
 const verbose = new Verbose();
 
+
+function stripDonts(input: string) {
+  const regex = /(don't\(\)[\s\S]*?do\(\))/g;   //FRUSTRATED ... used [\s\S]* not [\s\S]*? ... hour debugging :-(
+  input = input.replace(regex, '');
+  //remove any tailing don't()
+  const index = input.indexOf("don't()");
+  if (index >=0 ) {
+    input = input.substring(0, index);
+  }
+  // const trailingDont = /(don't\(\)[\s\S]*)$/
+  // input = input.replace(trailingDont, '');
+  return input;
+}
+
+function parseValidCommands(input: string) {
+  const regex = /(mul\(\d{1,3},\d{1,3}\))/g;
+  const matches = input.matchAll(regex);  
+  return Array.from(matches, (m) => m[0]);
+}
+function execCommand(command: string): number {
+  command = command.substring('mul('.length); //remove front
+  command = command.substring(0, command.length-1);
+  return command
+    .split(',')
+    .reduce((sum, str) => sum*parsers.toIntAssert(str), 1);
+}
+
+
+
 export async function day3b(dataPath?: string) {
   const data = (await readData(dataPath)).filter(Boolean);
-  return data.reduce((sum, curr) => sum + 1, 0);  
+
+    //combine into one line of text and strip out doNots
+    const cleanData = stripDonts(data.join(''));
+    const commands = parseValidCommands(cleanData);
+
+    //console.log("Commands: ", commands);
+
+    return commands.reduce((sum, cmd) => sum + execCommand(cmd), 0);
+
+    //10345261
+  
 }
 
 Verbose.setActive(false);
